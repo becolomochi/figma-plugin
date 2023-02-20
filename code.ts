c59a0f987cd3e5ab1602bc871d70df62479e7a34
becolomochi <becolomochi@gmail.com>
2023-02-20 15:59:29 +0900

    四角・円・三角形を描画してみる

--- code.ts
+++ code.ts
@@ -17,9 +17,13 @@ figma.ui.onmessage = msg => {
   if (msg.type === 'create-rectangles') {
     const nodes: SceneNode[] = [];
     for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
+      // const rect = figma.createRectangle();
+      // const rect = figma.createEllipse();
+      const rect = figma.createPolygon();
       rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      rect.y = i * 150;
+
+      rect.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
